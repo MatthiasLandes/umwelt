0e65029f77865913d17bec65637643595c03eeca
vinzzenzz <vinzenz.senftl@gmail.com>
2023-08-29 16:44:49 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,28 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 import GoogleMapReact from 'google-map-react';
-import React from "react";
+import React, { useState }from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import tasks from "../src/tasks.json";
+import Info from "./Info"
 
 // {defaultProps.center}
 
 function App() {
 
-  const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-  const defaultProps = {
-    center: {
-      lat: 48.137154,
-      lng: 11.576124
-    },
-    zoom: 13
-  };
-
-  const state = {
-    center: [51.505, -0.091],
-    zoom: 13,
-  };
+  const [id, setId] = useState(0);
 
   return (
     <div className="App">
@@ -36,22 +24,34 @@ function App() {
         <p>
           2000 Euro Fördergeld
         </p>
+        <div id="wrapper">
+        <div id="div1">
+        <Info id={id}></Info>
+        </div>
 
-        // (Important! Always set the container height explicitly)
-          <div style={{ height: '100vh', width: '100%' }}>
+          <div id="div2">
           <MapContainer center={[48.137154, 11.576124]} zoom={16} scrollWheelZoom={false}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             {tasks.map((task) => (
-            <Marker position={task.loc}>
+            <Marker 
+              position={task.loc} 
+              eventHandlers={{
+                click: (e) => {
+                  setId(task.id)
+                },
+              }}
+              >
               <Popup>
                 {task.name}
               </Popup>
+              
             </Marker>
             ))}
           </MapContainer>
+          </div>
           </div>
       </header>
     </div>
